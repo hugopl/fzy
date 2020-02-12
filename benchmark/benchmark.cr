3d2fee3772905beeaf5f87b5600ca84ce0fd1232
Hugo Parente Lima <hugo.pl@gmail.com>
2020-02-12 15:07:30 -0300

    Add more benchmarks, for prepared haystack and empty searches.

--- benchmark/benchmark.cr
+++ benchmark/benchmark.cr
@@ -4,7 +4,9 @@ require "../src/fzy"
 haystack = File.read("./benchmark/data.txt").split("\n")
 # prepared_haystack = Fzy::PreparedHaystack.new(haystack)
 
+prepared_haystack = Fzy::PreparedHaystack.new(haystack)
 Benchmark.ips do |x|
   x.report("bare search") { Fzy.search("main", haystack) }
-  # x.report("prepared search") { FuzzyMatch.search("main", prepared_haystack) }
+  x.report("empty search") { Fzy.search("", haystack) }
+  x.report("prepared search") { Fzy.search("main", prepared_haystack) }
 end
