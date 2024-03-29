5e19e94bc33ce8a4e5c0eba55025f35290f93f9c
Hugo Parente Lima <hugo.pl@gmail.com>
2024-03-29 19:01:02 -0300

    Update benchmark script.

--- benchmark/benchmark.cr
+++ benchmark/benchmark.cr
@@ -1,3 +1,4 @@
+#!/bin/env -S crystal run --release
 require "benchmark"
 require "../src/fzy"
 
@@ -5,12 +6,6 @@ haystack = File.read("./benchmark/data.txt").split("\n")
 
 prepared_haystack = Fzy::PreparedHaystack.new(haystack)
 Benchmark.ips do |x|
-  x.report("bare search") do
-    Fzy.search("main", haystack)
-    Fzy.search("MAIN", haystack)
-  end
-  x.report("prepared search") do
-    Fzy.search("main", prepared_haystack)
-    Fzy.search("MAIN", prepared_haystack)
-  end
+  x.report("bare search") { Fzy.search("main", haystack) }
+  x.report("prepared search") { Fzy.search("main", prepared_haystack) }
 end
